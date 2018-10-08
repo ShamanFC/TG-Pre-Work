242a49640a9ae6afb245153a627a42efd8c40175
g00py <41312392+ShamanFC@users.noreply.github.com>
2018-10-08 15:00:12 -0600

    JS - Higher Order Functions

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Higher-Order-Functions/Functions-as-Data.js
@@ -0,0 +1,14 @@
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+// Write your code below
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+is2p2();
+console.log(is2p2.name);
+
+// you can save functions the same way you save any other data type. you can redefine a function as a shorter named variable and call it the same way to save space and keep your code clean!
